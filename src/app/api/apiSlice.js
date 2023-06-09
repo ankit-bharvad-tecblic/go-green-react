97729e286afe53a350b80764e95ef7bab1a15b99
tecblic2022 <ankit.bharvad@tecblic.com>
2023-06-09 11:38:43 +0530

    redux and api setup done

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logOut } from "../../features/auth/authSlice";
+import { localStorageService } from "../../services/localStorge.service";
+
+const baseQuery = fetchBaseQuery({
+  // baseUrl: "http://192.168.0.124:8000/",
+  baseUrl: process.env.REACT_APP_API_BASE_URL_LOCAL,
+  // credentials: "include",
+  mode: "cors",
+
+  prepareHeaders: (headers, { getState }) => {
+    const token = localStorageService.get("GG_ADMIN")?.token;
+
+    console.log("api main slice log ---> ", token);
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.originalStatus === 403) {
+    console.log("sending refresh token");
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/refresh", api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user;
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      // retry the original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({}),
+});
---
+++ src/constants/api.constants.js
@@ -0,0 +1,14 @@
+export const API = {
+  SIGNUP: "/register/",
+  LOGIN: "/login/",
+  ADD_USER: "/create_admin_user/",
+  GET_COUNTRY: "/hotel/country_data/",
+  GET_CITY: "/hotel/city_data",
+
+  DASHBOARD: {
+    OVERALL_PERFORMANCE: "/booking/overall_performance/",
+    PACKAGE_PERFORMANCE: "/booking/package_dashboard/",
+    HOTEL_PERFORMANCE: "/booking/hotel_dashboard/",
+    SIGHTSEEING_PERFORMANCE: "/booking/sightseeing_dashboard/",
+  },
+};
